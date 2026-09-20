b7487d5a3e1f3c2e8cae6d8fddd58df22e4d7ca0
Nathan <nathanielorozco42@gmail.com>
2026-09-20 14:04:32 -0500

    Music!

--- burned-out/Scripts/is_shadow.gd
+++ burned-out/Scripts/is_shadow.gd
@@ -6,13 +6,35 @@ class_name SunManager extends Node
 @onready var sun_animation: AnimationPlayer = $"../DirectionalLight3D/AnimationPlayer"
 @onready var eclipse_sprite: AnimatedSprite3D = $"../eclipse"
 
+
+@onready var audio: AudioStreamPlayer = $AudioStreamPlayer 
+@onready var dummy_audio = AudioStreamPlayer.new()
+var fading = false
+
 static var is_sun_out: bool
 static var is_player_hidden: bool
 
 func _ready() -> void:
 	is_sun_out = false
-	#for testing. Comment in normal gameplay
 	moon_phase()
+	
+	add_child(dummy_audio)
+	audio.stream = load("res://Assets/Music/Dead of Night.ogg")
+	audio.play()
+	
+func _process(delta: float) -> void:
+	if (fading):
+		audio.volume_db -= 60 * delta
+		dummy_audio.volume_db += 60 * delta
+		
+		if (dummy_audio.volume_db >= 0 ):
+			audio.volume_db = 0
+			dummy_audio.volume_db = -60
+			audio.stream  = dummy_audio.stream
+			audio.play(dummy_audio.get_playback_position())
+			
+			dummy_audio.stop()
+			fading = false
 	
 func reset_phase() -> void:
 	sun_timer.stop()
@@ -29,7 +51,7 @@ func moon_phase():
 	is_sun_out = false
 	sun_animation.play_backwards("RotateSun")
 	eclipse_sprite.play("moon_phase")
-	
+	play_song("Dead of Night")
 	moon_timer.start()
 
 func sun_phase():
@@ -37,5 +59,15 @@ func sun_phase():
 	is_sun_out = true
 	sun_animation.play("RotateSun")
 	eclipse_sprite.play("sun_phase")
-	
+	play_song("Ortis Solis")
 	sun_timer.start()
+
+
+func play_song(song_name) -> void:
+	dummy_audio.stream = load("res://Assets/Music/" + song_name + ".ogg")
+	dummy_audio.volume_db = -60
+	dummy_audio.play()
+	
+	fading = true	
+	
+	
